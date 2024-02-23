250a21d46dd55768bb2ca31146657faedba018f1
DoLan662003 <153400698+DoLan662003@users.noreply.github.com>
2024-02-23 10:38:08 +0700

    Add files via upload

---
+++ postgres.sql
@@ -0,0 +1,10 @@
+SELECT * FROM public.students
+ORDER BY id ASC 
+
+ALTER TABLE students ADD COLUMN username VARCHAR(255);
+
+SELECT * FROM public.otps
+ORDER BY id ASC 
+
+SELECT * FROM public.users
+ORDER BY id ASC 
